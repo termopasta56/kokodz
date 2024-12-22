5273ac2470c691fef0d5a15ca6c70411d7a1b28b
termopasta56 <vladslada85@gmail.com>
2024-12-22 10:36:55 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import turtle
+help(turtle)
+
+for method in dir(turtle):
+    print(method)
+# speed - керує шввидкістю
+# pensize - керує розміром пера
+# pencolor - керує кольором пера
+# pendown - опускає перо вниз
+# penup - піднімає перо вверх
